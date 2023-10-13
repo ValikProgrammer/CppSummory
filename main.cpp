d75717a1ff9ddbb2edce247768ce61998d261480
Vector <ValikProgrammer@gmail.com>
2023-10-13 09:19:52 +0300

    upgraded C++ template

--- main.cpp
+++ main.cpp
@@ -1,26 +1,52 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
 
-vector<int> subset;
-int n = 4;
-void search(int k) {
-    if (k == n+1) {
-    // обработать подмножество
-        for (auto i : subset) {
-            cout << i << " ";
-        }
-    } else {
-    // включить k в подмножество
-    subset.push_back(k);
-    search(k+1);
-    subset.pop_back();
-    // не включать k в подмножество2.2. Рекурсивные алгоритмы  33
-    search(k+1);
-    }
+// #define debug(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s);istream_iterator<string> _it(_ss); err(_it, args); }
+// void err(istream_iterator<string> it) {cout<<"\n";}
+// template<typename T, typename... Args>
+// void err(istream_iterator<string> it, T a, Args... args) {
+//     cerr << *it << " = " << a <<"  ,  ";
+//     err(++it, args...);
+// }
+
+#define debug(args...) {string _s = #args;replace(_s.begin(),_s.end(),',',' ');stringstream _ss(_s);istream_iterator<string> _it(_ss);err(_it,args);}
+void err(istream_iterator<string> it) { cout << "\n";}
+template<typename T,typename... Args>
+void err(istream_iterator<string> it, T a, Args... args) {
+    cerr << *it << " = " << a << " ";
+    err(++it,args...);
 }
 
+
+// #define deb(args...) { \
+//     string _s = #args; \
+//     replace(_s.begin(), _s.end(), ',', ' '); \
+//     cout << _s << "\n";\
+// }
+
+
+    
+//     //stringstream _ss(_s);istream_iterator<string> _it(_ss); err(_it, args); }
+// void err(istream_iterator<string> it) {cout<<endl;}
+// template<typename T, typename... Args>
+// void err(istream_iterator<string> it, T a, Args... args) {
+//     cerr << *it << " = " << a <<"  ,  ";
+//     err(++it, args...);
+// }
+
+
 int main() {
-    search(2);
+    string s  = "abacaba"; 
+    string s2 = "dubaduba";
+    string s3 = "chego ty smotrish?";
+    cout << s << "\n";
+    int n = 100;
+    vector<int> v= {1,2,3,4};
+    debug(&v);
+    // debug(s,s2,n,s3,"hello");
+
+
+
+
 }
--- templateCpp.cpp
+++ templateCpp.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 #define cr(res) cout << res << "\n";
-#define debug(x) cerr << #x << " = " << x << "\n";
+// #define debug(x) cerr << #x << " = " << x << "\n";
 #define pb(a) push_back(a)
 #define mp(a,b) make_pair(a,b)
 #define X first
@@ -17,8 +17,8 @@ using namespace std;
 #define printvp(v) each(i,v) { cout << i.X << " " << i.Y << "\n"} // check 
 #define iv(v,x) find(v.begin(),v.end(),x)-v.begin();
 #define riv(v,x) szv(v) - (find(v.rbegin(),v.rend(),x) -v.rbegin()) 
-// #define maxE(vec) *max_element(vec.begin(),vec.end());
-// #define minE(vec) *min_element(vec.begin(),vec.end());
+#define maxE(vec) *max_element(vec.begin(),vec.end())
+#define minE(vec) *min_element(vec.begin(),vec.end())
 #define Code ios_base::sync_with_stdio(false);
 #define By cin.tie(0);
 #define Iron_man47 cout.tie(0);
@@ -40,20 +40,21 @@ typedef map<int,int> mii;
 typedef set<int> si;
 typedef set<char> sc;
 
-#define endl "\n"
 #define nn "\n"
 #define um unordered_map
 #define us unordered_set
 #define FAST ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 #define len(x) x.size()
 #define all(x) x.begin(),x.end()
-
+#define _ << " " <<
 const int INF = (int)1e9;
 const int MOD = (int)1e9+7;
 // bool cmpr(const vi &a,const vi &b)  { 
 //     return (a[2] < b[2]); 
 // } 
 
+
+// ==== INPUT aNd OUTPUT
 template <typename T, typename V > void pn( pair<T,V> x) { cout << "{" << x.first << " , " << x.second << "}\n";}
 template <typename T, typename V > void ps( pair<T,V> x) { cout << "{" << x.first << " , " << x.second << "} ";}
 template <typename T> void pn( T x)  { cout  << x << "\n";}
@@ -61,23 +62,54 @@ template <typename T> void ps( T x)  { cout  << x << " ";}
 template <typename T> void pn( T *x) {for (auto i : *x ) {ps(  i ); }cout << "\n"; }
 template <typename T> void pn2(T *x) {for (auto i : *x ) {for (auto j : i) {cout << j << " ";}cout << "\n";}}
 
+// cin
+template <class T> istream& operator >> (istream& in, vector<T>& v) { for (auto& e : v) { in >> e; } return in; }
+template <class T,class V> istream& operator >> (istream& in, vector<pair<T,V>>& v) { for (auto& e : v) { in >> e.first >> e.second; } return in; }
+template <class T> istream& operator >> (istream& in, pair<T, T>& v) { in >> v.first >> v.second; return in; }
 
-void solve () {
-    
+// for set and vector are the same 
+template <class T> ostream& operator << (ostream& out, const vector<T>& v) { for (auto& e : v) { out << e << " "; }  return out; }
+template <class T> ostream& operator << (ostream& out, const set<T>& v) { for (auto& e : v) { out << e << " "; }  return out; }
+// pair 
+template <class T, class V> ostream& operator << (ostream& out, const pair<T, V>& v) { out << "{ " << v.first << ":" << v.second << " }"; return out; }
+// double vector and map
+template <class T, class V> ostream& operator << (ostream& out, const vector< pair<T, V>> & v) { for (auto& e : v) { out << "{ " << e.first << ":" << e.second << " }" << "\n"; }  return out; }
+template <class T, class V> ostream& operator << (ostream& out, const map< T, V> & v) { for (auto& e : v) { out << e.first << ":" << e.second << "\n"; }  return out; }
+// it can print 2d vector, but there is no \n , so add new template
+template <class T> ostream& operator << (ostream& out, const vector<vector<T>>& v) { for (auto& e : v) { for (auto& ee : e ) {out << ee << "-"; } out << "\n";}  return out; }
+
+#define debug(args...) {string _s = #args;replace(_s.begin(),_s.end(),',',' ');stringstream _ss(_s);istream_iterator<string> _it(_ss);err(_it,args);}
+void err(istream_iterator<string> it) { cout << "\n";}
+template<typename T,typename... Args>
+void err(istream_iterator<string> it, T a, Args... args) {
+    cout << *it << " = " << a << " ";
+    err(++it,args...);
+}
+// ==== INPUT aNd OUTPUT
+
+
+
+void precalc() {
+
+}
+
+
+void solve() {
+    int n ;cin >> n;
 }
 
 
 int main ( ) {
-    Code By Iron_man47
-
+    // Code By Iron_man47
     freopen("in.txt","r",stdin);
     // freopen("out.txt","w",stdout);
-    int t = 1;
-    cin >> t;
 
+    int t = 1;
+    // cin >> t;
     while ( t--) {
         solve();
     }
 
     return 0;
 }
+
